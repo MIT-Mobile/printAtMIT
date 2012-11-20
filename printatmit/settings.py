65ef28747ecc863e1c748bc34f59d3b30154f682
root <root@mobile-print-dev.MIT.EDU>
2012-11-20 01:32:21 -0500

    preparing for backend and android merge(backend)

--- printatmit/settings.py
+++ printatmit/settings.py
@@ -46,7 +46,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = "/home/printapi/media_static/"
+MEDIA_ROOT = "/home/printapi/printatmit/media_static/"
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -57,7 +57,7 @@ MEDIA_URL = '/static_media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/printapi/staticfiles/'
+STATIC_ROOT = '/home/printapi/printatmit/staticfiles/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -108,7 +108,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/printapi/templates',
+    '/home/printapi/printatmit/templates',
 )
 
 INSTALLED_APPS = (
